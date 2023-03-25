07177e038a62193c9d5005e3ab55bc8734144c0f
Felix Schmitz <felixschmitz57@gmail.com>
2023-03-25 15:06:12 +0100

    Changes in params for first model

--- source/model_params.py
+++ source/model_params.py
@@ -1,16 +1,19 @@
 parameters={
     'seed':40,
+    'mode': 1, # with standard mode [0], diagnostics test hree [1], and diagnostics test adaptation [2]
+    # 'dividendSequence': None, # for mode 2; diagnostics test with given dividend sequence
+    # 'priceSeries': None, # for mode 2; diagnostics test with given price series
     'N':25, # num of agents & num of assets
     'steps':100, # num of steps/iterations by the model
-    'average_dividend':10, # \bar{d}
-    'autoregressive_parameter':0.95, # in the paper rho
-    'error_variance':0.0743,
+    'averageDividend':10, # \bar{d}
+    'autoregressiveParam':0.95, # in the paper rho
+    'errorVar':0.0743,
     'dorra':0.5, #degree of relative risk aversion
-    'interest_rate':0.1,
+    'interestRate':0.1,
     'C': 0.005, # cost levied for specificity
-    'initial_price_dividend_variance':4.0,
+    'initialPriceDividendVariance':4.0,
     'M': 100, # number of predictors per agent
-    'forecast_adaptation': 1, # binary with 0 for slow and 1 for fast
-    'initial_cash':20000, # initial cash of each agent in the bank
-    'trials_specialist':10, # trials per timestep for market clearing
+    'forecastAdaptation': 1, # binary with 0 for slow and 1 for fast
+    # 'initial_cash':20000, # initial cash of each agent in the bank
+    # 'trialsSpecialist':10, # trials per timestep for market clearing
 }
