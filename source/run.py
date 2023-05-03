4f6c7045554fb1677b20111276ffbc14614c5219
Felix Schmitz <felixschmitz57@gmail.com>
2023-05-03 10:22:10 +0200

    🎨  black formatting

--- source/run.py
+++ source/run.py
@@ -6,27 +6,30 @@ from viz_helper import price_lineplot
 import agentpy as ap
 import matplotlib.pyplot as plt
 
-def running_experiment(params: dict=parameters, model: ap.Model=ASM) -> ap.DataDict:
+
+def running_experiment(params: dict = parameters, model: ap.Model = ASM) -> ap.DataDict:
     exp_params = params
-    exp_params.update({'forecast_adaptation': ap.Values(0, 1)})
+    exp_params.update({"forecast_adaptation": ap.Values(0, 1)})
     exp_sample = ap.Sample(exp_params, randomize=False)
     exp = ap.Experiment(model, exp_sample, iterations=1, record=True, randomize=False)
     exp_results = exp.run(n_jobs=-1)
     return exp_results
 
-def running_model(params: dict=parameters, model: ap.Model=ASM) -> ap.DataDict:
+
+def running_model(params: dict = parameters, model: ap.Model = ASM) -> ap.DataDict:
     m = model(params)
     results = m.run()
     return results
 
 
-if __name__ == '__main__':
-    #experiment_results = running_experiment()
-    #print(experiment_results['info'])
+if __name__ == "__main__":
+    # experiment_results = running_experiment()
+    # print(experiment_results['info'])
     model_results = running_model()
-    #print(model_results['info'])
-    #print(model_results['variables']['ArtificialStockMarket'])
-    data = model_results['variables']['ArtificialStockMarket'][['hreePrice', 'marketPrice']]
+    # print(model_results['info'])
+    # print(model_results['variables']['ArtificialStockMarket'])
+    data = model_results["variables"]["ArtificialStockMarket"][
+        ["hreePrice", "marketPrice"]
+    ]
     fig = price_lineplot(data)
     plt.show()
-
