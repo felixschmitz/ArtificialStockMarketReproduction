6bf0f9652fa36f93b8a3d03bdd0ce2a852e0a46f
Felix Schmitz <felixschmitz57@gmail.com>
2023-05-17 15:53:54 +0200

    mostly setup, step and documentation function changes

--- source/model.py
+++ source/model.py
@@ -4,43 +4,96 @@ import agentpy as ap
 import numpy as np
 import math
 
+
 class ArtificialStockMarket(ap.Model):
-    """def __init__(self):
-        pass"""
+    """
+    def __init__(self, parameters=None, _run_id=None, **kwargs):
+        super().__init__(parameters, _run_id, **kwargs)
+    """
 
     def setup(self: ap.Model):
         self.dividend = self.p.averageDividend
-        self.hreeSlope, self.hreeIntercept, self.hreeVariance = self.hree_values()
-        self.hreePrice = self.hree_price()
+        self.currentPrice = 100
+        # self.hreeSlope, self.hreeIntercept, self.hreeVariance = self.hree_values()
+        # self.hreePrice = self.hree_price()
         self.agents = ap.AgentList(self, self.p.N, MS)
+        self.varPriceDividend = 4.0
 
     def step(self: ap.Model):
-        #print(self.agents.U(10))
         self.dividend = self.dividend_process()
+
+        """print(self.agents.U(10))
+        
         self.hreePrice = self.hree_price()
-        self.marketPrice = self.market_clearing_price()
+        self.marketPrice = self.market_clearing_price()"""
         self.document()
 
     def document(self: ap.Model):
-        self.record(self.vars)
+        self.record(
+            [
+                "dividend",
+                "currentPrice",
+            ]
+        )
+        self.varPriceDividend = np.var(
+            np.array(self.log.dividend) + np.array(self.log.currentPrice)
+        )
+        self.record(["varPriceDividend"])
 
     def dividend_process(self: ap.Model) -> float:
-        '''Returns dividend based on AR(1) process given average dividend, autoregressive parameter, and error variance.'''
+        """Returns dividend based on AR(1) process given average dividend,
+        autoregressive parameter, and error variance."""
         errorTerm = self.nprandom.normal(0, math.sqrt(self.p.errorVar))
-        return self.p.averageDividend + self.p.autoregressiveParam * (self.dividend - self.p.averageDividend) + errorTerm
-    
-    def hree_values(self: ap.Model, a_min: float=0.7, a_max: float=1.2, b_min: float=-10, b_max: float=19.002):
-        '''Returns homogeneous rational expectations equilibrium predictos values.'''
-        return self.nprandom.uniform(a_min, a_max), self.nprandom.uniform(b_min, b_max), self.p.initialPriceDividendVariance
-    
+        return (
+            self.p.averageDividend
+            + self.p.autoregressiveParam * (self.dividend - self.p.averageDividend)
+            + errorTerm
+        )
+
+    def specialist(self: ap.Model):
+        """If the specialist is not able to find a market clearing price in the first place,
+        an iterative process is started in which new trial prices are announced and agents update their
+        effective demands and partial derivatives accordingly. If complete market clearing is not reached
+        within a specified number of trials, one side of the market will be rationed.
+        """
+        pass
+
+    """def hree_values(
+        self: ap.Model,
+        a_min: float = 0.7,
+        a_max: float = 1.2,
+        b_min: float = -10,
+        b_max: float = 19.002,
+    ):
+        ""Returns homogeneous rational expectations equilibrium predictor values.""
+        return (
+            self.nprandom.uniform(a_min, a_max),
+            self.nprandom.uniform(b_min, b_max),
+            self.p.initialPriceDividendVariance,
+        )
+
     def hree_price(self: ap.Model) -> float:
-        '''Returns homogeneous rational expectation equilibrium price.'''
-        f = self.p.autoregressiveParam / (1 + self.p.interestRate - self.p.autoregressiveParam)
-        g = ((1 + f) * ((1 - self.p.autoregressiveParam) * self.p.averageDividend - self.p.dorra * self.p.errorVar)) / self.p.interestRate
+        ""Returns homogeneous rational expectation equilibrium price.""
+        f = self.p.autoregressiveParam / (
+            1 + self.p.interestRate - self.p.autoregressiveParam
+        )
+        g = (
+            (1 + f)
+            * (
+                (1 - self.p.autoregressiveParam) * self.p.averageDividend
+                - self.p.dorra * self.p.errorVar
+            )
+        ) / self.p.interestRate
         return f * self.dividend + g
-    
+
     def market_clearing_price(self: ap.Model) -> float:
-        '''Returns inductive market clearing price.'''
-        numerator = self.dividend * (sum(self.agents.slope) / sum(self.agents.pdVariance)) + (sum(self.agents.intercept) / sum(self.agents.pdVariance)) - self.p.N * self.p.dorra
-        denominator = ((1 + self.p.interestRate) * (1 / sum(self.agents.pdVariance)) - (sum(self.agents.slope) / sum(self.agents.pdVariance)))
-        return numerator / denominator
+        ""Returns inductive market clearing price.""
+        numerator = (
+            self.dividend * (sum(self.agents.slope) / sum(self.agents.pdVariance))
+            + (sum(self.agents.intercept) / sum(self.agents.pdVariance))
+            - self.p.N * self.p.dorra
+        )
+        denominator = (1 + self.p.interestRate) * (1 / sum(self.agents.pdVariance)) - (
+            sum(self.agents.slope) / sum(self.agents.pdVariance)
+        )
+        return numerator / denominator"""
