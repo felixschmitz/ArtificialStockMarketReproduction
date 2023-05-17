e892f1c72f54f615315f840981bbd0661fff3689
Felix Schmitz <felixschmitz57@gmail.com>
2023-05-17 15:52:52 +0200

    placed plot on ax object

--- source/viz_helper.py
+++ source/viz_helper.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+
 def price_lineplot(data: pd.Series) -> plt.Figure:
     fig, ax = plt.subplots()
-    ax = data.plot()
+    data.plot(ax=ax)
     return fig
