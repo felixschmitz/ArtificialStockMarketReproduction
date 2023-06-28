8fb35de5a19a74cf7824d7bae5f6c8f1b5eebca4
Felix Schmitz <felixschmitz57@gmail.com>
2023-06-28 12:08:35 +0200

    🎨  lineplot with error area

--- source/viz_helper.py
+++ source/viz_helper.py
@@ -7,3 +7,11 @@ def lineplot(data: pd.Series) -> plt.Figure:
     fig, ax = plt.subplots()
     data.plot(ax=ax)
     return fig
+
+
+def errLineplot(data: pd.DataFrame, y: str, err: str) -> plt.Figure:
+    fig = lineplot(data=data[y])
+    plt.fill_between(
+        data.index.to_list(), data[y] - data[err], data[y] + data[err], alpha=0.5
+    )
+    return fig
