af9f71543de71b06c7373032526f62bef8ca8642
Felix Schmitz <felixschmitz57@gmail.com>
2023-08-25 16:32:54 +0200

    💩 minor changes to model parameters

--- source/model_params.py
+++ source/model_params.py
@@ -1,10 +1,10 @@
 parameters = {
-    "importPath": r"results/ASM_50000_21072023-144813",  # path to innate rules
+    "importPath": r"results/ASM_50000_23082023-175615",  # path to innate rules
     "seed": 42,
-    "forecastAdaptation": 1,  # binary with 0 for slow/hree and 1 for fast/complex
+    "forecastAdaptation": 0,  # binary with 0 for slow/hree and 1 for fast/complex
     "mode": 0,  # with standard mode [0], diagnostics test "clamped" hree predictors [1],
     # diagnostics test hree adaptation [2], and innating rules (pre-trained rules) [3]
-    "steps": 2.5e5,  # num of steps/iterations by the model
+    "steps": 5e4,  # 2.5e5,  # num of steps/iterations by the model
     "N": 25,  # num of agents & num of assets
     "averageDividend": 10,  # \bar{d}
     "autoregressiveParam": 0.95,  # in the paper rho
@@ -16,9 +16,10 @@ parameters = {
     "M": 100,  # number of predictors per agent
     "initialCash": 20000,  # initial cash of each agent in the bank (cf. Ehrentreich2008 p.94)
     "minCash": 0,  # minimum cash of each agent in the bank
-    "epsilon": 1e-1,  # maximum deviation in specialist's market clearing
+    "epsilon": 1e-2,  # maximum deviation in specialist's market clearing
     "trialsSpecialist": 10,  # trials per timestep for market clearing
     # "specialistType": 1,  # 0: ration expectations, 1: slope, 2: fixed ETA; default: 1
+    "maxBid": 10,  # maximum bid of each agent to the risky-asset
     "minPrice": 0.01,  # minimum price for market clearing
     "maxPrice": 500,  # maximum price for market clearing
     "minHolding": 0,  # minimum holding for market clearing
